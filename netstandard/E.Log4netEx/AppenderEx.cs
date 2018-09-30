f14cb0bb8ea69d7607bfa943e1d31b916d91918b
stan e <yi.hang@live.com>
2018-09-30 09:59:19 +0800

    将log4net standard版本修改为2.0,并添加扩展项目

---
+++ netstandard/E.Log4netEx/AppenderEx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Data.Sqlite;
+using Pomelo.Data.MySql;
+using Npgsql;
+
+namespace E.Log4netEx
+{
+    /// <summary>
+    /// Log4net Appender extension
+    /// </summary>
+    public static class AppenderEx
+    {
+        /// <summary>
+        /// Create database connection
+        /// </summary>
+        /// <param name="connectionString">Database connectionString</param>
+        /// <param name="connectionType">Database connectionType(sqlserver,pgsql,sqlite,mysql;default value 'sqlserver')</param>
+        /// <returns>Database connection instance</returns>
+        public static IDbConnection CreateConnection(string connectionString, string connectionType = "sqlserver")
+        {
+            IDbConnection connection = null;
+            switch (connectionType.ToLower())
+            {
+                case "mysql":
+                    connection = new MySqlConnection();
+                    break;
+                case "pgsql":
+                    connection = new NpgsqlConnection();
+                    break;
+                case "sqlite":
+                    connection = new SqliteConnection();
+                    break;
+                case "sqlserver":
+                default:
+                    connection = new SqlConnection();
+                    break;
+            }
+            connection.ConnectionString = connectionString;
+            return connection;
+        }
+    }
+}
